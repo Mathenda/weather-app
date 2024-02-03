6a45f34186802d4078e13df027ace7da562182c2
The1Don1 <mathendam@gmail.com>
2024-02-03 14:37:31 +0200

    added first linear gradient

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,19 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="flex column">    <!-- so everything on the page is stacked -->
+
   </q-page>
 </template>
 
 <script>
+
 import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'IndexPage'
 })
 </script>
+
+<style lang="sass">
+.q-page
+  background: linear-gradient(to bottom, #f0f2f0, #000c40) !important
+</style>
